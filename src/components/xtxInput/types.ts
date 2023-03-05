88e3f9aa2ac69c9eab31947264e1bb39f5014936
par-zengzijie <894817389@qq.com>
2023-03-06 00:01:28 +0800

    feat: docs文档

--- src/components/xtxInput/types.ts
+++
@@ -1,2 +0,0 @@
-export type _InputConfirmType = 'send' | 'search' | 'next' | 'go' | 'done';
-export type _InputType = 'text' | 'number' | 'idcard' | 'digit' | 'tel' | 'safe-password' | 'nickname';
--- src/pages/goods/list.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <XtxResult />
-</template>
-
-<script lang="ts" setup>
-import XtxResult from '@/components/XtxResult.vue';
-</script>
-
-<style lang="scss"></style>
